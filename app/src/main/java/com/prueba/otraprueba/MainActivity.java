034b609b651996dc62685608037b20e8fed01c86
dacner <dacnerpiedrahita@gmail.com>
2023-02-20 19:04:07 -0500

    Primer commit

---
+++ app/src/main/java/com/prueba/otraprueba/MainActivity.java
@@ -0,0 +1,18 @@
+package com.prueba.otraprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    String TAG = "Depuracion";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "Estoy en Oncreate");
+    }
+}
